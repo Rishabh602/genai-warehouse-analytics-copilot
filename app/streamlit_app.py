ccc5f7c675ff679ccf5bb694075de39362b864f0
Rish <rishabhbansal602@gmail.com>
2026-05-23 17:59:11 +0200

    Fix Streamlit Cloud imports

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -14,25 +14,20 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 
-# Add src folder path
-
-sys.path.append("../src")
-
-
 # Import project engines
 
-from kpi_engine import kpi_rules
+from src.kpi_engine import kpi_rules
 
-from anomaly_engine import detect_anomaly
+from src.anomaly_engine import detect_anomaly
 
-from summary_engine import (
+from src.summary_engine import (
     create_dynamic_summary,
     extract_top_bottom_n
 )
 
-from ai_engine import ask_warehouse_copilot
+from src.ai_engine import ask_warehouse_copilot
 
-from chart_engine import (
+from src.chart_engine import (
     detect_chart_intent,
     create_chart_data,
     create_dynamic_chart
